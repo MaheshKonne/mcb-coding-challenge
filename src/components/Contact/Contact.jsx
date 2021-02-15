57bab5e1e05ff0b707298c2b08292e8ce636eeaa
MaheshKonne7 <mahesh.konne@everest.engineering>
2021-02-15 13:51:45 +0530

    add validation to contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ import InputField from '../ui-components/InputField';
 import Title from '../ui-components/Title';
 import './Contact.scss';
 import contactImg from '../../assets/Illustration.png';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import DateInput from '../ui-components/DateInput';
 import Dropdown from '../ui-components/Dropdown';
 
@@ -12,10 +12,13 @@ const Contact = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [dob, setDob] = useState('');
+    const [designation, setDesignation] = useState('');
 
-    const onDesignationChange = selectedDesignation => {
-        console.log(`Selected designation is ${selectedDesignation}`);
-    }
+    const isValidEmail = useCallback(() => {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+    }, [email]);
+
+    const shouldEnableCheckbox = name.length >= 3 && isValidEmail(email) && dob && designation;
 
     return (
         <div className="contact">
@@ -27,13 +30,13 @@ const Contact = () => {
                 <InputField label="Name" value={name} onChange={setName} />
                 <InputField label="Email address" value={email} onChange={setEmail} />
                 <DateInput label="Date of birth" value={dob} onChange={setDob} />
-                <Dropdown label="Designation" options={['Tesla', 'Volvo', 'Mercedes']} onChange={onDesignationChange} />
-                <Checkbox>
+                <Dropdown label="Designation" options={['Tesla', 'Volvo', 'Mercedes']} onChange={setDesignation} />
+                <Checkbox disabled={!shouldEnableCheckbox}>
                     <span>
                         Accept the <a className="underlined" href="#">Terms & Conditions</a>
                     </span>
                 </Checkbox>
-                <Button variant="contained">Submit</Button>
+                <Button variant="contained" onClick={() => {}}>Submit</Button>
             </div>
         </div>        
     );
--- src/components/ui-components/Checkbox.jsx
+++ src/components/ui-components/Checkbox.jsx
@@ -1,9 +1,9 @@
 import './Checkbox.scss';
 
-const Checkbox = ({ children }) => {
+const Checkbox = ({ children, disabled }) => {
     return (
         <div className="checkbox">
-            <input type="checkbox" id="checkbox" name="" value="" />
+            <input type="checkbox" id="checkbox" name="" value="" disabled={disabled} />
             <label htmlFor="checkbox">
             {children}
             </label>
